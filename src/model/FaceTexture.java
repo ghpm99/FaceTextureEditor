957e5418fc1fd884075be6b1d2954fa9f877253b
Guilherme Henrique <59860892+ghpm99@users.noreply.github.com>
2020-12-17 22:16:12 -0300

    FaceTexture

---
+++ src/model/FaceTexture.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author ghpm9
+ */
+public class FaceTexture {
+    private String name;
+    private BufferedImage image;
+    private int order;
+    private String classe;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+    
+    
+}
